0865d055d596f883609381d606e889ab32f826a2
OrbinWaffle <79740022+OrbinWaffle@users.noreply.github.com>
2024-06-27 11:02:53 -0700

    timer and win screen

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,20 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Car : MonoBehaviour
 {
     public float speed;
+    public float thresholdSpeed;
+    public float trapTime;
+    bool isTrapped;
+    float startTrapTime;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = Vector3.zero;
+        rb.inertiaTensorRotation = Quaternion.identity;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (rb.velocity.magnitude<thresholdSpeed & !isTrapped)
+        {
+            isTrapped = true;
+        }
+        if (rb.velocity.magnitude > thresholdSpeed & isTrapped)
+        {
+            isTrapped = false;
+        }
+        if (!isTrapped)
+        {
+            startTrapTime = Time.time;
+        }
+        if(Time.time - startTrapTime > trapTime)
+        {
+            GameManager.main.OnWinCondition();
+        }
 
     }
     private void FixedUpdate()
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager main;
+    bool hasWon = false;
+    public TextMeshProUGUI winText;
+    public TextMeshProUGUI timeText;
+    public float startTime;
+    float currentTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        main = this;
+    }
+    private void Update()
+    {
+        if (!hasWon)
+        {
+            currentTime = Time.time - startTime;
+            timeText.text = "Time: " + currentTime.ToString("#.##");
+        }
+    }
+    public void OnWinCondition()
+    {
+        if(!hasWon)
+        {
+            hasWon = true;
+            winText.gameObject.SetActive(true);
+            winText.text = "You caught the robbers in " + currentTime.ToString("#.##") + " seconds!";
+        }
+    }
+}
