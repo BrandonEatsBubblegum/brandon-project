ebed091c499a2bb10a4fc2d27bea2da532f03e7e
BeandonLu0223 <156550046+BeandonLu0223@users.noreply.github.com>
2024-07-11 10:59:59 -0700

    spam explosion

--- Assets/Scripts/Brandon's Stuff/Missile.cs
+++ Assets/Scripts/Brandon's Stuff/Missile.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
-    public float speed;
+    public float startSpeed;
+    public float acceleration;
     public GameObject target;
+    public GameObject explosion;
+    float speed;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         target = GameObject.FindGameObjectWithTag("Robber");
+        speed = startSpeed;
     }
 
     // Update is called once per frame
@@ -19,7 +23,15 @@ public class Missile : MonoBehaviour
     {
         Vector3 targetVector = (target.transform.position - transform.position).normalized;
         gameObject.transform.up = targetVector;
-
+        speed += acceleration * Time.fixedDeltaTime;
         rb.AddForce(transform.up * speed, ForceMode.Acceleration);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Robber"))
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
